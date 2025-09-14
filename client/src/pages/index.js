7c2594f748bd049b7467a794d380ac91938ee57a
chakrabortyanish <chakrabortyanish981@gmail.com>
2025-09-14 17:30:23 +0530

    file update

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,6 @@ import Home from './home/Home.jsx';
 import SignUp from './signUp/SignUp.jsx';
 import LogIn from './logIn/LogIn.jsx';
 import Cart from './cart/Cart.jsx';
-import Delete from './deleteAccount/delete.jsx';
+import Delete from './deleteAccount/Delete.jsx';
 
 export { Home, SignUp, LogIn, Cart, Delete};
