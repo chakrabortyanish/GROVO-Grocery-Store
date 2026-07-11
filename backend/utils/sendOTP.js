6f7c9a37a49d0b785e5ff2b340f323538f9e91df
chakrabortyanish <chakrabortyanish981@gmail.com>
2026-07-11 10:33:38 +0530

    update host

--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -5,12 +5,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.APP_PASSWORD
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.APP_PASSWORD,
+  },
 });
 
 const sendOTP = async (email, otp) => {
